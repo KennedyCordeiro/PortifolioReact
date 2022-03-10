9fb9b66781c908002206064b608007983af2d7d7
Kennedy Cordeiro <kennedy.rocha@fitbank.com.br>
2022-03-10 14:23:14 -0300

    Aplicação Navbar e fundo

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,30 +1,34 @@
 import React from 'react'
 import 'bootstrap/dist/css/bootstrap.min.css';
+import logo from "../logo.png";
 
 const Navbar = () => {
-    return (
-        <div className='container-fluid'>
-            <nav class="navbar-fixed navbar navbar-expand-lg navbar-light bg-light">
-                <a class="navbar-brand" href="#">Navbar</a>
-                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                    <span class="navbar-toggler-icon"></span>
-                </button>
 
-                <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                    <ul class="navbar-nav mr-auto">
-                        <li class="nav-item active">
-                            <a class="nav-link" href="#">Home <span class="sr-only"></span></a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="#">Link</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link disabled" href="#">Disabled</a>
-                        </li>
-                    </ul>
-                </div>
-            </nav>
-        </div>
+    return (
+        <nav className="navbar navbar-expand-lg navbar-dark fixed-top" id="mainNav">
+            <a className="navbar-brand" href="#"><img src={logo} className="img img-logo" alt="logo..." ></img></a>
+            <button className="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+
+            <div className="collapse navbar-collapse" id="navbarResponsive">
+                <ul className="navbar-nav ms-auto text-uppercase" id="NavItem">
+                    <li className="nav-item active">
+                        <a className="nav-link js-scroll-trigger" href="#services">Serviços</a>
+                    </li>
+                    <li className="nav-item">
+                        <a className="nav-link js-scroll-trigger" href="#portfolio">Portfolio</a>
+                    </li>
+                    <li className="nav-item">
+                        <a className="nav-link js-scroll-trigger" href="#about">Sobre</a>
+                    </li>
+                    <li className="nav-item">
+                        <a className="nav-link js-scroll-trigger" href="#contact">Contato</a>
+                    </li>
+                </ul>
+            </div>
+        </nav>
+     
     );
 }
 
