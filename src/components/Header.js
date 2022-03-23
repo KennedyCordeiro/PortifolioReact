6cdfbf8c7ebbfd4ec4aba0847ebcde3771c62622
Kennedy Cordeiro <kennedy.rocha@fitbank.com.br>
2022-03-23 12:30:09 -0300

    Adição das alterações de Main e adição do Header e dependencias do typed React

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,20 @@
-import React from 'react'
-import Typed from 'react-typed'
+import React from 'react';
+import Typed from 'react-typed';
+
 const Header = () => {
     return (
         <div className='header-wraper'>
-                <div className='main-info'>
-                    <h1>Web development and Computer solutions</h1>
+
+                <div className='main-info intro-content'>
+                    <h1>Criação e Manutenção de Computadores</h1>
+                    <Typed
+                        className='typed-text'
+                        strings={["Web development and Computer solutions", "Full Stack Developer", "Computer Building 🚀"]}
+                        typeSpeed={50}
+                        backSpeed={70}
+                        loop
+                    />
+                    <a href = '#' className=".btn-main-offer"/>
                 </div>
             </div>
     );
