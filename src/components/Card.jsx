b805ef8cd5d6b7573a1f10636a61267780669d1e
“Kennedy <kennedy.rocha@fitbank.com.br>
2022-04-08 13:26:03 -0300

    Adicionado fundo do Header dos cards, a resolvido o problema de tamanho de cards.

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -2,21 +2,21 @@ import React, { Component } from "react";
 import Card from "./CardUI";
 import "bootstrap/dist/css/bootstrap.min.css";
 import img1 from '../img/setup.jpg'
-import img2 from '../img/code.png'
+import img2 from '../img/code.jpg'
 import img3 from '../img/Man.jpg'
 class Cards extends Component {
   render() {
     return (
-      <div className="container-fluid d-flex justify-content-center">
+      <div className="container-fluid d-flex justify-content-center card-header">
         <div className="row">
           <div className="col-md-4">
-            <Card imgsrc ={img1} />
+            <Card imgsrc ={img1} title = 'Computer Builder' />
           </div>
           <div className="col-md-4">
-            <Card imgsrc ={img2} />
+            <Card imgsrc ={img2} title='Full Stack Developer' />
           </div>
-          <div className="col-md-4">
-            <Card imgsrc ={img3} />
+          <div className="col-md-4" >
+            <Card imgsrc ={img3} title='Computer Maintenance'/>
           </div>
         </div>
       </div>
--- src/components/CardUI.jsx
+++ src/components/CardUI.jsx
@@ -1,18 +1,24 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import code from './../img/code.png';
 import './../css/Cards.css'
 import '../../node_modules/bootstrap/dist/css/bootstrap.min.css'
 
 const CardUI = props => {
+    // [tags, SetTags] = useState({tags:[]})
+
+    // const handleTag = () => {
+    //     SetTags({})
+    // }
+
+
     return (
 
         <div className='card text-center'>
             <div className='overflow'>
                 <img src={props.imgsrc} alt='Image 1' className='card-img-top'/>
             </div>
-            <div className='card-body text-dark'>
-                <h4 className='card-title'>Web Developer</h4>
+            <div className='card-body'>
+                <h4 className='card-title'>{props.title}</h4>
                 <p className='card-text text-secondary'>Web developer at Fitbank 450 and The Marias Beauty</p>
                 <a href = '#' className='btn btn-card btn-outline-sucess'>About more</a>
             </div>
